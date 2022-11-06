01c096aa269714d818b6f6237a35b8ea5c6f8d46
nico <nbreval@isotrol.com>
2022-11-06 17:18:02 +0100

    FIX - Add variable to control stacktrace return on controllers' exceptions

--- src/main/kotlin/org/nitb/orchestrator2/controller/exception/ExceptionHandler.kt
+++ src/main/kotlin/org/nitb/orchestrator2/controller/exception/ExceptionHandler.kt
@@ -1,19 +1,26 @@
 package org.nitb.orchestrator2.controller.exception
 
 import io.micronaut.context.annotation.Requires
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Produces
 import io.micronaut.http.server.exceptions.ExceptionHandler
 import jakarta.inject.Singleton
 import org.nitb.orchestrator2.model.ControllerExceptionMessage
+import org.slf4j.LoggerFactory
 
 @Produces
 @Singleton
 @Requires(classes = [Exception::class, ExceptionHandler::class])
-class ExceptionHandler:
-    ExceptionHandler<Exception, HttpResponse<*>> {
+class ExceptionHandler(
+    @Value("\${orchestrator.exception.show-trace}")
+    val showTrace: Boolean
+): ExceptionHandler<Exception, HttpResponse<*>> {
     override fun handle(request: HttpRequest<*>?, exception: Exception?): HttpResponse<*> {
-        return HttpResponse.ok(ControllerExceptionMessage(exception?.message ?: "", exception?.stackTrace ?: arrayOf()))
+        logger.error(exception?.message, exception)
+        return HttpResponse.ok(ControllerExceptionMessage(exception?.message ?: "", if (showTrace) exception?.stackTrace ?: arrayOf() else arrayOf()))
     }
+
+    private val logger = LoggerFactory.getLogger("exception-handler")
 }
